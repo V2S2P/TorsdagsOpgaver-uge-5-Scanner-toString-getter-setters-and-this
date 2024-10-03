5fc951cc0d6ad90cdf7776d40355c0ce5ed9e214
V2S2P <157969468+V2S2P@users.noreply.github.com>
2024-10-03 12:05:02 +0200

    Update Main.java

--- Task 5/Main.java
+++ Task 5/Main.java
@@ -1,22 +1,29 @@
 import java.util.*;
 public class Main{
 	public static void main(String[] args){
-		ArrayList<String> actions = new ArrayList<>();
+		ArrayList<String> actions = new ArrayList<>(); // Initialize ArrayList of type String.
+		// Adding Strings to the ArrayList "actions".
 		actions.add("1) Start game");
 		actions.add("2) Resume game");
 		actions.add("3) Pause game");
 		actions.add("4) End game");
 
+		// Creating a "GameMenu" object that we call "game". The "actions" list is passed to its constructor (parameter) to initialize the menu.
 		GameMenu game = new GameMenu(actions);
 
+		// Call the displayMenu() method on the "game" object, which will print all the options in the "actions" list.
 		game.displayMenu();
 
+		// Call the getAction() method, which allows the user to input their choice. The user input is then stored in the "choice" variable as a String.
 		String choice = game.getAction();
 
+		// Converting the String input from the user into an int by using the "Integer.parseInt()". This integer will then represent the choice from the user.
 		int userChoice = Integer.parseInt(choice);
 
+		// Call the doAction() method and passing the "userChoice" integer as parameter. The method will run the the code that corresponds to the input from the userChoice.
 		doAction(userChoice);
 	}
+	// This method will execute code depending on the "action" parameter, which is the user's choice.
 	public static void doAction(int action){
 		switch(action){
 		case 1:
